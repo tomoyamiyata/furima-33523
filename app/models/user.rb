d77a7d6e41d4537930f5b72f0412406d88c01f11
tomoyamiyata <tomoya.0310mdesu@gmail.com>
2021-01-28 14:33:48 +0900

    ユーザー管理機能var3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-
-         validates :birthday, presence: true 
-         validates :nick_name, presence: true
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
-         validates :last_name, presence: true, format:    {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現 
-         validates :first_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
-         validates :last_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
          
         
+        with_options presence: true do
+          validates :birthday
+          validates :nick_name
+          validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
+          validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現 
+          validates :first_kana, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
+          validates :last_kana, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
+          validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "英数字文字6以上"}
+          
+
+          
+        end
+
+        #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+          #validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
          
 
-         VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-         validates :password, format: { with: VALID_PASSWORD_REGEX }
-
-
+     
+         
+        
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  #devise_for :users
-
-  get 'items/index'
   root to: "items#index"
  
   
