2873fe724f17d0ee466b35fda3b6617716ebd079
tomoyamiyata <tomoya.0310mdesu@gmail.com>
2021-02-04 19:01:08 +0900

    商品情報編集機能var1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
     @item = Item.includes(:user).order('created_at DESC')
   end
@@ -23,12 +25,32 @@ class ItemsController < ApplicationController
      @item = Item.find(params[:id])
      
   end
-  
 
+  def edit
+    @item = Item.find(params[:id])
+    
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :product_condition_id, :shipping_charge_id, :delivery_area_id, :estimated_shipping_dat_id, :category_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
