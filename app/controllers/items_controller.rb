32e14974b0dff4c0d33e5e2e9ac2633ab6fa9286
tomoyamiyata <tomoya.0310mdesu@gmail.com>
2021-02-05 11:41:20 +0900

    商品情報編集機能var2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,10 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    
+    if  current_user.id != @item.user_id 
+      redirect_to item_path(@item)
+    end
+     
   end
   
   def update
